0ed2cdbc74a2bfc07ed1d2013ca4bbe123867613
Andre Dezain <Andre Dezain>
2016-05-03 10:23:16 +0700

    commit3

--- js/isotopex.js
+++ js/isotopex.js
@@ -1,9 +1,17 @@
 // init Isotope
 var $grid = $('.workzxliz').isotope({
-  // options
+  itemSelector: '.grid-item',
+  percentPosition: true,
+  masonry: {
+    columnWidth: '.grid-item'
+  }
 });
 // filter items on button click
 $('.workzx').on( 'click', 'button', function() {
   var filterValue = $(this).attr('data-filter');
   $grid.isotope({ filter: filterValue });
+});
+
+$grid.imagesLoaded().progress( function() {
+  $grid.isotope('layout');
 });
